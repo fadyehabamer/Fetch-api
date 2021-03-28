b04b30cd8106376d041cb9b5184805f95a113347
fadyehabamer <fadyamer45@gmail.com>
2021-03-28 17:56:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-var $slider = $("#slider")
-var $fill = $(".bar .fill")
+function fetchdata() {
+  fetch("https://reqres.in/api/users")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const appdata = data.data
+        .map((getusers) => {
+          return `
+          <div class= "data">
+          <p> this is ${getusers.first_name} <br></p>
+          <p> this is his her avatar<br> <img src="${getusers.avatar}"></p>
+          <p> and this is his / her email <br> ${getusers.email}  </p>
+          </div> `;
+        })
+        .join("");
 
-function setBar() {
-    $fill.css("width", $slider.val() + "%")
+      document.querySelector("#app").insertAdjacentHTML("beforebegin", appdata);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
-$slider.on("input", setBar)
-
-setBar()
+fetchdata();
